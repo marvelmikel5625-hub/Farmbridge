f0781d5abf564c4a7b3daea909ace9ebb1058f5c
marvelmikel5625-hub <marvelmikel5625@gmail.com>
2026-06-14 11:38:50 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ajtybnpyfrclzdipgyeb.supabase.co';
-const supabaseAnonKey = sb_publishable_3PGOCU2gR3tEpaTUR-a75g_GOJu_sfj
+const supabaseAnonKey = 'sb_publishable_3PGOCU2gR3tEpaTUR-a75g_GOJu_sfj';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
